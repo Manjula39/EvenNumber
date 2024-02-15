f9099c5093a8b2831dd689086bfe8fd318911b0c
Manjula36 <manjulavdt@gmail.com>
2024-02-15 15:21:31 +0530

    Add all Files

---
+++ EvenNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogValues
+{
+    public class EvenNumbers
+    {
+     public static void main(string[] args)
+        {
+            for (int i = 2;  i<=100; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Display even number from 1 to 100 ");
